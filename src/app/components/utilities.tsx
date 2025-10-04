be1abb63f2736d656e8e011ce261687a89ef4fa2
karimbobboi <abdulkarimbobboi@gmail.com>
2025-10-04 18:14:34 +0100

    expand utilities file

--- src/app/components/utilities.tsx
+++ src/app/components/utilities.tsx
@@ -1,30 +1,72 @@
 type BadgeType = "news" | "press" | "video" | "external";
+type BadgeSize = "sm" | "md" | "lg";
 
 type Badge = {
 	label: string;
 	bgcolour: string; //tailwind class for bg color
 	textcolour: string; //tailwind class for text color
+	ringcolour: string; //tailwind class for inset ring color
+}
+
+export interface NewsItem {
+	id: number;
+	title: string;
+	date: string;
+	category: BadgeType;
+	image?: string;
+	video?: string;
+	description?: string;
+	source?: string;
+	url?: string;
 }
 
 export const NewsBadges: Record <BadgeType, Badge> = {
 	news: {
 		label: "NEWS",
 		bgcolour: "bg-green-100",
-		textcolour: "text-green-800"
+		textcolour: "text-green-900",
+		ringcolour: "inset-ring-green-900"
 	},
 	press: {
 		label: "PRESS RELEASE",
 		bgcolour: "bg-blue-100",
-		textcolour: "text-blue-800"
+		textcolour: "text-blue-900",
+		ringcolour: "inset-ring-blue-900"
 	},
 	video: {
 		label: "VIDEO",
-		bgcolour: "bg-gray-100",
-		textcolour: "text-gray-800"
+		bgcolour: "bg-red-100",
+		textcolour: "text-red-900",
+		ringcolour: "inset-ring-red-900"
 	},
 	external: {
 		label: "EXTERNAL COVERAGE",
 		bgcolour: "bg-gray-100",
-		textcolour: "text-gray-800"
+		textcolour: "text-gray-800",
+		ringcolour: "inset-ring-green-900"
 	}
+};
+
+const badgeSizes: Record<BadgeSize, string> = {
+	sm: "px-2 py-0.5 text-[0.6rem]",
+	md: "px-2.5 py-1 text-sm",
+	lg: "px-3 py-1.5 text-base"
+};
+
+interface BadgeProps {
+	type: BadgeType;
+	size?: BadgeSize;
+}
+
+export const Badge = ({ type, size = "sm" }: BadgeProps) => {
+	const badge = NewsBadges[type];
+    const sizeClasses = badgeSizes[size];
+  
+  return (
+    <span 
+      className={`inline-flex items-center rounded-full font-medium inset-ring ${sizeClasses} ${badge.bgcolour} ${badge.textcolour} ${badge.ringcolour}`}
+    >
+      {badge.label}
+    </span>
+  );
 };
